f3b5269181064df79a1f97ca8a38f508f3000a11
Harshit <harshitbhardwaj001@gmail.com>
2024-01-13 04:24:45 +0530

    frontend

---
+++ pages/contact.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_9gwzr2h",
+        "template_ap73vof",
+        form.current,
+        "h8q0CHU-G5Dv2QR7V"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message Sent!!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Not able to send the message currently!!");
+        }
+      );
+  };
+  return (
+    <div class="flex items-center justify-center p-12">
+      <div class="mx-auto w-full max-w-[550px]">
+        <form action="https://formbold.com/s/FORM_ID" ref={form}>
+          <div class="mb-5">
+            <label
+              for="name"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Full Name"
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000] focus:shadow-md"
+            />
+          </div>
+          <div class="mb-5">
+            <label
+              for="email"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@domain.com"
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000] focus:shadow-md"
+            />
+          </div>
+          <div class="mb-5">
+            <label
+              for="subject"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Subject
+            </label>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              placeholder="Enter your subject"
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000] focus:shadow-md"
+            />
+          </div>
+          <div class="mb-5">
+            <label
+              for="message"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Message
+            </label>
+            <textarea
+              rows="4"
+              name="message"
+              id="message"
+              placeholder="Type your message"
+              class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000] focus:shadow-md"
+            ></textarea>
+          </div>
+          <div>
+            <button
+              class="hover:shadow-form rounded-md bg-[#000] py-3 px-8 text-base font-semibold text-white outline-none"
+              onClick={sendEmail}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default contact;
